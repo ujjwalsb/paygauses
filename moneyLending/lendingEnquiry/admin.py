2df9099b9834eac8b1cbc3a52ef713a97a9bcede
ujjwalsb <ujjwalsingh15@gmail.com>
2020-06-18 16:24:06 +0530

    admin

--- moneyLending/lendingEnquiry/admin.py
+++ moneyLending/lendingEnquiry/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import paygausesEnquiry
 
-# Register your models here.
+class PayModelAdmin(admin.ModelAdmin):
+    list_display = ["firstName", "email", "mobile", "loanAmount"]
+    list_display_links = ["firstName"]
+    #list_editable = ["link"]
+    list_filter = ["loanNeedTime", "revenue", "registeredAs", "ageOfBusiness"]
+    search_fields = ["firstName", "lastName", "email", "mobile"]
+
+    class Meta:
+        model = paygausesEnquiry
+
+
+admin.site.register(paygausesEnquiry, PayModelAdmin)
