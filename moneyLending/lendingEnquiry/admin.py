735b57a70809b6433d8f4fda17e32d63a9ca25cc
ujjwalsb <ujjwalsingh15@gmail.com>
2020-06-18 16:28:51 +0530

    admin

--- moneyLending/lendingEnquiry/admin.py
+++ moneyLending/lendingEnquiry/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import paygausesEnquiry
 
 class PayModelAdmin(admin.ModelAdmin):
-    list_display = ["firstName", "email", "mobile", "loanAmount"]
-    list_display_links = ["firstName"]
+    list_display = ["firstName", "email", "mobile", "loanAmount", "loanNeedTime"]
+    list_display_links = ["firstName", "email", "mobile", "loanAmount", "loanNeedTime"]
     #list_editable = ["link"]
     list_filter = ["loanNeedTime", "revenue", "registeredAs", "ageOfBusiness"]
     search_fields = ["firstName", "lastName", "email", "mobile"]
